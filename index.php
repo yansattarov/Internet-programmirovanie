5662d666a3e6b3568ff5ed97b2a9a88eb6f4d47e
yansattarov <81461070+yansattarov@users.noreply.github.com>
2021-12-23 21:55:30 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-﻿<p> Дата и время:
+<p> Дата и время:
 <p>
 <?php
  $d=date("d.m.Y H:i");
@@ -6,48 +6,95 @@
 ?> 
 <ul>
         <li>
-           <a href="http://php_sattarov/lab-1-1.php">Упражнение 1-2</a>
+           <a href="http://a0609079.xsph.ru/lab-1-1.php">Упражнение 1-2</a>
 		   </li>
 		   <li>
-		   <a href="http://php_sattarov/lab-1-2.php">Упражнение 1-3</a>
+		   <a href="http://a0609079.xsph.ru/lab-1-2.php">Упражнение 1-3</a>
 		   </li>
 		   <li>
-		   <a href="http://php_sattarov/lab-1-3.php">Упражнение 1-4</a>
+		   <a href="http://a0609079.xsph.ru/lab-1-3.php">Упражнение 1-4</a>
         </li>
 		    <li>
-		   <a href="http://php_sattarov/lab1-.php">Задача 1-1</a>
+		   <a href="http://a0609079.xsph.ru/lab1-5.php">Задача 1-1</a>
         </li>
 				    <li>
-		   <a href="http://php_sattarov/lab-2-1.php">Упражнение 1-6</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-1.php">Упражнение 1-6</a>
         </li>
 						    <li>
-		   <a href="http://php_sattarov/lab-2-2.php">Упражнение 1-7</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-2.php">Упражнение 1-7</a>
         </li>
 								    <li>
-		   <a href="http://php_sattarov/lab-2-3.php">Упражнение 1-8</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-3.php">Упражнение 1-8</a>
         </li>
 										    <li>
-		   <a href="http://php_sattarov/lab-2-4.php">Упражнение 1-9</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-4.php">Упражнение 1-9</a>
         </li>
 										    <li>
-		   <a href="http://php_sattarov/lab-2-5.php">Упражнение 1-10</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-5.php">Упражнение 1-10</a>
         </li>
 										    <li>
-		   <a href="http://php_sattarov/lab-2-6.php">Упражнение 1-11</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-6.php">Упражнение 1-11</a>
         </li>
 										    <li>
-		   <a href="http://php_sattarov/lab-2-7.php">Упражнение 1-12</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-7.php">Упражнение 1-12</a>
         </li>
 										    <li>
-		   <a href="http://php_sattarov/lab-2-8.php">Упражнение 1-13</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-8.php">Упражнение 1-13</a>
         </li>
 										    <li>
-		   <a href="http://php_sattarov/lab-2-9.php">Упражнение 1-14</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-9.php">Упражнение 1-14</a>
         </li>
 												    <li>
-		   <a href="http://php_sattarov/lab-2-10.php">Задача 1-2</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-10.php">Задача 1-2</a>
         </li>
 														    <li>
-		   <a href="http://php_sattarov/lab-2-11.php">Задача 1-3</a>
+		   <a href="http://a0609079.xsph.ru/lab-2-11.php">Задача 1-3</a>
         </li>
+		
+		Лабораторная работа 2:
+																    <li>
+		   <a href="http://a0609079.xsph.ru/lab2-1.php">Задача 2.1</a>
+        </li>
+																		    <li>
+		   <a href="http://a0609079.xsph.ru/lab2-2.php">Задача 2.2</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/lab2-3.php">Задача 2.3</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/lab2-4.php">Задача 2.4</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/lab2-5.php">Задача 2.5</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/lab2-6.php">Задача 2.6</a>
+        </li>
+        Лабораторная работа 3:
+        																    <li>
+		   <a href="http://a0609079.xsph.ru/f1.html">Упражнение 3-1</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/f2.html">Упражнение 3-2</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/f3.html">Упражнение 3-3</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/f4.html">Упражнение 3-4</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/f5.html">Упражнение 3-5</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/f6.html">Упражнение 3-6</a>
+        </li>                												 <li>
+		   <a href="http://a0609079.xsph.ru/f7.php">Упражнение 3-7</a>
+        </li>																	    <li>
+		   <a href="http://a0609079.xsph.ru/s3-1.html">Задача 3-1</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/s3-2.php">Задача 3-2</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/s3-3.php">Задача 3-3</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/s3-4.php">Задача 3-4</a>
+        </li>																    <li>
+		   <a href="http://a0609079.xsph.ru/s3-5.php">Задача 3-5</a>
+        </li>                                                                   <li>
+		   <a href="http://a0609079.xsph.ru/s3-6.html">Задача 3-6</a>
+        </li>
+
  </ul>
+
+ 
